744d84cb62c7e63dadba9acecf2e46aa0d089253
TIm Buckley <timothyb89@gmail.com>
2013-02-25 20:06:07 -0700

    initial maven config

---
+++ PA2/src/main/java/edu/colostate/cs200/pa2/App.java
@@ -0,0 +1,13 @@
+package edu.colostate.cs200.pa2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
