bf41352d2dec3408d540859dc48017283383ba36
Steven <sburrell23@gmail.com>
2013-02-27 19:13:26 -0700

    Added some i/o methods to quicksortToFile to store words into an array from the specified input file, and (eventually) write the array to the output file. [Does no sorting yet]

--- PA2/src/main/java/Quicksort.java
+++ PA2/src/main/java/Quicksort.java
@@ -43,9 +43,35 @@ public class Quicksort {
 	// In the output data file, you should write only one word per line. 
 	// To separate lines, use a linefeed charactor, "\n" at the end of the line.
     public void quicksortToFile(String file_input, String file_output){
-	// complete your method here
+    	String[] words = new String[19911];
+    	storeInput(file_input,words);
+    	//
+    	//Sort array here.
+    	//
+    	writeOut(file_output,words);
+    }
+    
+    private void storeInput(String file_input,String[] store){
+    	Scanner fileScanner = null;
+    	try{fileScanner = new Scanner(new File(file_input));}	
+    	catch (FileNotFoundException e){e.printStackTrace();}
+    
+    	int loc = 0;
+    	while(fileScanner.hasNextLine()){
+    		store[loc] = fileScanner.nextLine();
+    		loc++;
+    	}
+    	fileScanner.close();
     }
 
+    private void writeOut(String file_output,String[] store){
+    	try {
+    		BufferedWriter out = new BufferedWriter(new FileWriter(new File(file_output)));
+    		for(int i = 0; i < store.length;i++)
+    			out.write(store[i] + "\n");
+    		out.close();	
+    	} catch (IOException e) {e.printStackTrace();}
+    }
 
 }
 
