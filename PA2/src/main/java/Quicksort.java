24a00f5e35a326693ce8c6f6aca5c39398b727d1
timothyb89 <timothyb89@gmail.com>
2013-03-09 18:39:19 -0700

    Quicksort partition() now uses a middle pivot properly. Also fixed some potential IO bugs and NullPointerExceptions

--- PA2/src/main/java/Quicksort.java
+++ PA2/src/main/java/Quicksort.java
@@ -1,107 +1,126 @@
+
 /**
- * CS 200 Colorado State University, Spring 2013
- * This class implements the quicksort algorithm for 
- * any Comparable objects
- * Please pick an item in the middle of array as the pivot
- * For example, if your array, a[] has n items, and your lo is 0
- * and your hi is n-1, a[(n-1-0)/2] should be the first pivot
- * item to be picked.
+ * CS 200 Colorado State University, Spring 2013 This class implements the
+ * quicksort algorithm for any Comparable objects Please pick an item in the
+ * middle of array as the pivot For example, if your array, a[] has n items, and
+ * your lo is 0 and your hi is n-1, a[(n-1-0)/2] should be the first pivot item
+ * to be picked.
  *
  */
-
 import java.io.*;
 import java.util.*;
 
 public class Quicksort {
-    public Quicksort(){
-    }
 
-    public void sort(Comparable[] a) {
-        sort(a, 0, a.length - 1);
-    }
+	public Quicksort() {
+	}
 
-    // quicksort the subarray from a[lo] to a[hi]
-    private void sort(Comparable[] a, int lo, int hi) {
-    	
-        if (lo >= hi) 
-        	return;
-        
-        int pivot = partition(a, lo, hi);
+	public void sort(Comparable[] a) {
+		// can't sort a null / single element / empty list
+		if (a == null || a.length <= 1) {
+			return;
+		}
+		
+		sort(a, 0, a.length - 1);
+	}
 
-        sort(a, lo, pivot);
-        sort(a, pivot+1, hi);
-    }
+	// quicksort the subarray from a[lo] to a[hi]
+	private void sort(Comparable[] a, int lo, int hi) {
+		if (lo >= hi) {
+			return;
+		}
 
-    // partition the subarray a[lo .. hi] by returning an index j
-    // so that a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
-    private int partition(Comparable[] a, int lo, int hi) {
+		int pivot = partition(a, lo, hi);
 
-    	Comparable pivot = a[((hi-1)-lo)/2];
-    	
-    	int l = lo,h = hi;
-    	pivot = a[lo];
-    	
-    	while (true) {
-    		//System.out.println(pivot);
-    		
-    		while (a[h].compareTo(pivot) >= 0 && l < h) 	
-    			h--;
-    		while (a[l].compareTo(pivot) < 0 && l < h) 
-    			l++;
-    		
-    		if (l < h) {
-    			Comparable temp = a[l];
-    			a[l] = a[h];
-    			a[h] = temp;
-    		}
-    		else return h;
-    	}
-    }
+		sort(a, lo, pivot - 1);
+		sort(a, pivot + 1, hi);
+	}
 
+	// partition the subarray a[lo .. hi] by returning an index j
+	// so that a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+	private int partition(Comparable[] a, int lo, int hi) {
+		int pivotIndex = lo + ((hi - 1) - lo) / 2;
+		Comparable pivotValue = a[pivotIndex];
 
-    // sort data from a file and store them to a file
-    // useful classes:
-    // java.io.BufferedReader
-    // java.io.DataInputStream
-    // java.io.FileInputStream
-    // java.io.FileWriter
-    // java.io.BufferedWriter 
+		// move the pivot to the end - we need to skip over it in order to
+		// sort properly
+		swap(a, pivotIndex, hi);
+
+		int l = lo;
+
+		// iterate over each element, skipping the last (temporary pivot)
+		for (int i = lo; i <= hi - 1; i++) {
+
+			// if the element at i is less than the pivot value, swap it
+			if (a[i].compareTo(pivotValue) < 0) {
+				swap(a, i, l);
+				l++;
+			}
+		}
+
+		// put the pivot back at the right position
+		swap(a, l, hi);
+
+		// return the new pivot index
+		return l;
+	}
+
+	private void swap(Comparable[] array, int a, int b) {
+		Comparable aVal = array[a];
+		Comparable bVal = array[b];
+
+		array[a] = bVal;
+		array[b] = aVal;
+	}
+
+	// sort data from a file and store them to a file
+	// useful classes:
+	// java.io.BufferedReader
+	// java.io.DataInputStream
+	// java.io.FileInputStream
+	// java.io.FileWriter
+	// java.io.BufferedWriter 
 	//
 	// In the output data file, you should write only one word per line. 
 	// To separate lines, use a linefeed charactor, "\n" at the end of the line.
-    public void quicksortToFile(String file_input, String file_output){
-    	
-    	Comparable[] words = new Comparable[19911];
-    	
-    	storeInput(file_input,words);
-    	
-    	sort(words);
+	public void quicksortToFile(String file_input, String file_output) {
+		Comparable[] words = storeInput(file_input);
 
-    	writeOut(file_output,words);
-    }
-    
-    public void storeInput(String file_input,Comparable[] store){
-    	Scanner fileScanner = null;
-    	try{fileScanner = new Scanner(new File(file_input));}	
-    	catch (FileNotFoundException e){e.printStackTrace();}
-    
-    	int loc = 0;
-    	while(fileScanner.hasNextLine()){
-    		store[loc] = fileScanner.nextLine();
-    		loc++;
-    	}
-    	fileScanner.close();
-    }
+		sort(words);
 
-    private void writeOut(String file_output,Comparable[] store){
-    	try {
-    		BufferedWriter out = new BufferedWriter(new FileWriter(new File(file_output)));
-    		for(int i = 0; i < store.length;i++)
-    			out.write(store[i] + "\n");
-    		out.close();	
-    	} catch (IOException e) {e.printStackTrace();}
-    }
+		writeOut(file_output, words);
+	}
+
+	public Comparable[] storeInput(String file_input) {
+		Scanner fileScanner = null;
+		try {
+			fileScanner = new Scanner(new File(file_input));
+			
+			// linked list for fast adding - we'll convert to an array later
+			List<String> ret = new LinkedList<String>();
+
+			while (fileScanner.hasNextLine()) {
+				ret.add(fileScanner.nextLine());
+			}
+			fileScanner.close();
+
+			return ret.toArray(new String[0]);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private void writeOut(String file_output, Comparable[] store) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(new File(file_output)));
+			for (int i = 0; i < store.length; i++) {
+				out.write(store[i] + "\n");
+			}
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
-
-
--- PA2/src/main/java/TestQuicksort.java
+++ PA2/src/main/java/TestQuicksort.java
@@ -1,13 +1,14 @@
+
 public class TestQuicksort {
 
-    public static void main(String [] args){
-	if (args.length<2){
-	    System.out.println("Please enter your command.");
-	    System.out.println("e.g. java TestQuicksort [InputFilePath][OutputFilePath]");
-	}else{
-	    Quicksort qs = new Quicksort();
-	    qs.quicksortToFile(args[0], args[1]);
-	    
-	}	
-    }
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("Please enter your command.");
+			System.out.println("e.g. java TestQuicksort [InputFilePath][OutputFilePath]");
+		} else {
+			Quicksort qs = new Quicksort();
+			qs.quicksortToFile(args[0], args[1]);
+
+		}
+	}
 }
